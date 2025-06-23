2cd47623fa0e323ec9a36c66d6357b0eb4a6b72f
Ahmed Hisham <ahmed.hisham123666@gmail.com>
2025-06-24 02:49:00 +0300

    Create AND_OR_XOR_XNOR_Top_Module.v

---
+++ AND_OR_XOR_XNOR_Top_Module/AND_OR_XOR_XNOR_Top_Module.v
@@ -0,0 +1,25 @@
+module AND_OR_XOR_XNOR_Top_Module (
+   input [3:0] A , B , 
+   input [1:0] Sel , 
+   output [3:0] Out 
+);
+
+wire [3:0] Out_1 , Out_2 , Out_3 , Out_4 ;  
+
+AND_OR_XOR_XNOR B_1 ( .A( A ) ,
+                      .B( B ) , 
+                      .Out_1( Out_1 ) ,
+                      .Out_2( Out_2 ) ,
+                      .Out_3( Out_3 ) ,
+                      .Out_4( Out_4 )  
+);
+
+Mux_4_to_1 M_1 (.A   ( Out_1 )  ,
+                .B   ( Out_2 ) ,
+                .C   ( Out_3 ) ,
+                .D   ( Out_4 )  ,
+                .Sel ( Sel ) ,
+                .out (Out) 
+               ) ;
+
+endmodule
